3e9e8b550080624625ee3074865503c5894b62ba
AbdulVahhab Abdurahmonov <developerabduvahob714@gmail.com>
2026-03-24 14:30:00 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "l9dihbld794r61qpbdjljhva";
+$user = "mysql";
+$password = "CfyF,le13!#";
+$db = "default";
+
+$conn = new mysqli($host, $user, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+while($row = $result->fetch_assoc()) {
+    echo $row["id"] . " - " . $row["name"] . " - " . $row["email"] . "<br>";
+}
+
+?>
